fadc75a9e34ca660fac0941cc2d10570d4a35c57
Flowmaster <ellyssin.gimhae@gmail.com>
2018-03-12 22:11:36 -0600

    Initial commit using the echo framework

---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"log"
+	"encoding/json"
+	"github.com/labstack/echo"
+)
+
+type Order struct {
+	orderID string 
+	apiKeyId string //Key id used for the order? Remember why we have this?
+	exchangeOrderID string
+	baseCurrency string // "BTC",
+    baseCurrencyLong string // "Bitcoin", //As above
+	marketCurrency string // "LTC",
+    marketCurrencyLong string // "Litecoin", //Only bittrex seems to have this, pass the short name if doesn't exist
+    minTradeSize string  //"0.001", //string
+    marketName string // "LTCBTC", //Convention is market+base this is our name
+    //marketPrice: "0.41231231", //String Last price from socket for the pair in the exchange
+    //?btcPrice: "0.41231231", //String This is a shortcut for me not to calculate we can discuss it
+    //?fiatPrice: "1.35",  //Stting This is a shortcut for me not to calculate we can discuss it          
+    exchange string // "binance"
+    exchangeMarketName string // "LTC-BTC", //Some exchanges put dash others reverse them i.e. BTCLTC, 
+	orderType string // limit, market, stop, fake_market, see above.
+	rate string //String
+	baseQuantity float64
+	quantity float64 // baseQuantity / rate
+	quantityRemaining float64 // how many 
+	side string // buy, sell
+	conditions string
+	status string //open, draft, closed,
+	createdAt int64 //integer
+}
+
+type OrderRequest struct {
+	ApiKeyId string `json:"apiKeyId"`
+	ExchangeMarketName string `json:"exchangeMarketName"`
+	MarketName string `json:"marketName"`
+	Side string `json:"side"`
+	OrderType string `json:"orderType"`
+	UnitPrice float64 `json:"unitPrice"`
+	Qauntity float64 `json:"quantity"`
+	Conditions string `json:"conditions"`
+}
+
+// e.GET("/users/:id", getUser)
+func getOrder(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+
+	return c.JSON(http.StatusOK, map[string]string{
+		"id": id,
+		"exchangeName": "amigonex",
+	})
+}
+
+func listOrders(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:" + team + ", member:" + member)
+}
+
+func postOrder(c echo.Context) error {
+	order := OrderRequest{}
+
+	defer c.Request().Body.Close()
+	b, err := ioutil.ReadAll(c.Request().Body)
+	if err != nil {
+		log.Printf("failed reading the request %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	err = json.Unmarshal(b, &order)
+	if err != nil {
+		log.Printf("failed to unmarshal %s", err)
+		return c.String(http.StatusInternalServerError, "")
+	}
+
+	log.Printf("this is your order: %#v", order)
+	return c.String(http.StatusOK, "post order!")
+} 
+
+func updateOrder(c echo.Context) error {
+	return c.String(http.StatusOK, "update it!")
+}
+
+func deleteOrder(c echo.Context) error {
+	return c.String(http.StatusOK, "delete it!")
+}
+
+func main() {
+	e := echo.New()
+
+	e.GET("/orders", listOrders)
+	e.POST("/orders", postOrder)
+	e.GET("/orders/:id", getOrder)
+	e.PUT("/orders/:id", updateOrder)
+	e.DELETE("/orders/:id", deleteOrder)
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":5000"))
+}
