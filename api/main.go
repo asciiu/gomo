bbc916b83a4e373ae94916de439518d846c42fe9
Flowmaster <ellyssin.gimhae@gmail.com>
2018-03-26 00:39:04 -0600

    Functioning docker build and run of api with connection to local dev postgres DB

--- api/main.go
+++ api/main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/asciiu/gomo/api/routes"
 	"github.com/asciiu/gomo/common/database"
 	_ "github.com/lib/pq"
 )
 
-const (
-	privateKeyPath = "keys/gomo-key-ecdsa"
-	publicKeyPath  = "keys/gomo-key-ecdsa.pub"
-)
-
 func checkErr(err error) {
 	if err != nil {
-		log.Println(err)
+		log.Printf("ERROR: %s", err)
 		panic(err)
 	}
 }
 
 func main() {
-	db, err := database.NewDB("postgres://postgres@localhost/gomo_dev?&sslmode=disable")
+	dbUrl := fmt.Sprintf("postgres://postgres@%s:%s/gomo_dev?&sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
+	fmt.Println(dbUrl)
+
+	db, err := database.NewDB(dbUrl)
 	checkErr(err)
 	defer db.Close()
 
