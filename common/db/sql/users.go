2e5f10c1a083d16e9b9456a41633b3a20546cb39
Flowmaster <ellyssin.gimhae@gmail.com>
2018-03-29 17:25:11 -0600

    Fix missing FindUserById function

--- common/db/sql/users.go
+++ common/db/sql/users.go
@@ -17,6 +17,17 @@ func FindUser(db *sql.DB, email string) (*models.User, error) {
 	return &u, nil
 }
 
+func FindUserById(db *sql.DB, userId string) (*models.User, error) {
+	var u models.User
+	err := db.QueryRow("SELECT id, first_name, last_name, email, email_verified, password_hash FROM users WHERE id = $1", userId).
+		Scan(&u.Id, &u.First, &u.Last, &u.Email, &u.EmailVerified, &u.PasswordHash)
+
+	if err != nil {
+		return nil, err
+	}
+	return &u, nil
+}
+
 func InsertUser(db *sql.DB, user *models.User) (*models.User, error) {
 	sqlStatement := `insert into users (id, first_name, last_name, email, email_verified, password_hash) values ($1, $2, $3, $4, $5, $6)`
 	_, err := db.Exec(sqlStatement, user.Id, user.First, user.Last, user.Email, user.EmailVerified, user.PasswordHash)
