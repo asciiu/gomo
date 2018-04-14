73abec096b3120942a35cd712d1d474c38e059bf
Flowmaster <ellyssin.gimhae@gmail.com>
2018-04-13 22:12:05 -0600

    Added enum type for exchange names.

---
+++ common/models/types.go
@@ -0,0 +1,45 @@
+package models
+
+type Exchange int
+
+const (
+	Binance  Exchange = 0
+	Bittrex  Exchange = 1
+	Poloniex Exchange = 2
+	Unknown  Exchange = 3
+)
+
+func NewExchange(ex string) Exchange {
+	switch ex {
+	case "Binance":
+		return Binance
+	case "Bittrex":
+		return Bittrex
+	case "Poloniex":
+		return Poloniex
+	default:
+		return Unknown
+	}
+}
+
+func (exchange Exchange) String() string {
+	// ... operator counts how many items in the array
+	names := [...]string{
+		"Binance",
+		"Bittrex",
+		"Poloniex",
+		"Cryptopia"}
+	// → `day`: It's one of the
+	// values of Weekday constants.
+	// If the constant is Sunday,
+	// then day is 0.
+	// prevent panicking in case of
+	// `day` is out of range of Weekday
+	if exchange < Binance || exchange > Poloniex {
+		return "Unknown"
+	}
+	// return the name of a Weekday
+	// constant from the names array
+	// above.
+	return names[exchange]
+}
---
+++ common/models/types_test.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"testing"
+)
+
+func TestExchangeModelNew(t *testing.T) {
+	exchange := NewExchange("Binance")
+
+	if exchange != Binance {
+		t.Errorf("Binance != Binance enum")
+	}
+}
+
+func TestExchangeModelUnknown(t *testing.T) {
+	exchange := NewExchange("Kex")
+
+	if exchange != Unknown {
+		t.Errorf("unknown exchange failed")
+	}
+}
+
+func TestExchangeModelStringConversion(t *testing.T) {
+	exchange := NewExchange("Poloniex")
+
+	if exchange.String() != "Poloniex" {
+		t.Errorf("to string conversion failed")
+	}
+}
