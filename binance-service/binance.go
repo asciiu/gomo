f7883baf038b8fc93f25f1da8eaa87a1d1f9bc6f
Flowmaster <ellyssin.gimhae@gmail.com>
2018-04-15 23:13:50 -0600

    Balance request in binance should be in a go routine

--- binance-service/binance.go
+++ binance-service/binance.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"os"
 	"time"
@@ -11,39 +12,48 @@ import (
 	"github.com/go-kit/kit/log"
 )
 
-type KeyValidator struct{}
+type KeyValidator struct {
+	DB *sql.DB
+}
 
 func (sub *KeyValidator) Process(ctx context.Context, key *kp.ApiKey) error {
-	var logger log.Logger
-	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
-	logger = log.With(logger, "time", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
-
-	hmacSigner := &binance.HmacSigner{
-		Key: []byte(key.Secret),
-	}
-	//ctx, _ := context.WithCancel(context.Background())
-	// use second return value for cancelling request when shutting down the app
-
-	binanceService := binance.NewAPIService(
-		"https://www.binance.com",
-		key.Key,
-		hmacSigner,
-		logger,
-		ctx,
-	)
-	b := binance.NewBinance(binanceService)
-	request := binance.AccountRequest{
-		RecvWindow: time.Duration(2) * time.Second,
-		Timestamp:  time.Now(),
+	if key.Exchange != "Binance" {
+		return nil
 	}
 
-	account, error := b.Account(request)
-	if error != nil {
-		fmt.Printf("error encountered: %s", error)
-	}
+	go func() {
+		var logger log.Logger
+		logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+		logger = log.With(logger, "time", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
 
-	for i, balance := range account.Balances {
-		fmt.Printf("%d %s :: FREE: %f LOCKED %f\n", i, balance.Asset, balance.Free, balance.Locked)
-	}
+		hmacSigner := &binance.HmacSigner{
+			Key: []byte(key.Secret),
+		}
+
+		binanceService := binance.NewAPIService(
+			"https://www.binance.com",
+			key.Key,
+			hmacSigner,
+			logger,
+			ctx,
+		)
+		b := binance.NewBinance(binanceService)
+		request := binance.AccountRequest{
+			RecvWindow: time.Duration(2) * time.Second,
+			Timestamp:  time.Now(),
+		}
+
+		account, error := b.Account(request)
+		if error != nil {
+			fmt.Printf("error encountered: %s", error)
+		}
+
+		// publish verify key event
+
+		// publish balances here an an event
+		for i, balance := range account.Balances {
+			fmt.Printf("%d %s :: FREE: %f LOCKED %f\n", i, balance.Asset, balance.Free, balance.Locked)
+		}
+	}()
 	return nil
 }
--- binance-service/main.go
+++ binance-service/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	msg "github.com/asciiu/gomo/apikey-service/models"
+	"github.com/asciiu/gomo/common/db"
 	micro "github.com/micro/go-micro"
 )
 
@@ -14,8 +17,15 @@ func main() {
 
 	srv.Init()
 
+	dbUrl := fmt.Sprintf("%s", os.Getenv("DB_URL"))
+	gomoDB, err := db.NewDB(dbUrl)
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
 	// subscribe to new key topic with a key validator
-	micro.RegisterSubscriber(msg.TopicNewKey, srv.Server(), new(KeyValidator))
+	micro.RegisterSubscriber(msg.TopicNewKey, srv.Server(), &KeyValidator{gomoDB})
 	if err := srv.Run(); err != nil {
 		log.Fatal(err)
 	}
