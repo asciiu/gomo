fc9b3023026eb48311a6486dde3973cc38e2e3b0
Flowmaster <ellyssin.gimhae@gmail.com>
2018-04-18 21:44:04 -0600

    Added currencyQuantity to OrderRequest proto. The order service will also handle buys and sells in their own functions.

--- order-service/orderService.go
+++ order-service/orderService.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	bp "github.com/asciiu/gomo/balance-service/proto/balance"
+	"github.com/asciiu/gomo/common/enums"
 	evt "github.com/asciiu/gomo/common/proto/events"
 	orderRepo "github.com/asciiu/gomo/order-service/db/sql"
 	pb "github.com/asciiu/gomo/order-service/proto/order"
@@ -19,12 +20,11 @@ type OrderService struct {
 	NewOrderPub micro.Publisher
 }
 
-func (service *OrderService) AddOrder(ctx context.Context, req *pb.OrderRequest, response *pb.OrderResponse) error {
-	// we will always assume the market trading pairs will be
-	// the currency-base currency: e.g. ADA-BTC
-	baseCurrency := strings.Split(req.MarketName, "-")[1]
+// Add a buy order
+func (service *OrderService) addBuyOrder(ctx context.Context, req *pb.OrderRequest, response *pb.OrderResponse) error {
+	currencies := strings.Split(req.MarketName, "-")
+	baseCurrency := currencies[1]
 
-	// is there enough balance
 	balRequest := bp.GetUserBalanceRequest{
 		UserId:   req.UserId,
 		ApiKeyId: req.ApiKeyId,
@@ -43,7 +43,7 @@ func (service *OrderService) AddOrder(ctx context.Context, req *pb.OrderRequest,
 
 	if balResponse.Data.Balance.Available < req.BaseQuantity {
 		response.Status = "fail"
-		response.Message = "insufficient balance " + baseCurrency
+		response.Message = "insufficient available balance: " + baseCurrency
 		return nil
 	}
 
@@ -77,6 +77,82 @@ func (service *OrderService) AddOrder(ctx context.Context, req *pb.OrderRequest,
 	default:
 		response.Status = "error"
 		response.Message = "ecountered error on Insert: " + error.Error()
+		return nil
+	}
+}
+
+// add a sell order
+func (service *OrderService) addSellOrder(ctx context.Context, req *pb.OrderRequest, response *pb.OrderResponse) error {
+	currencies := strings.Split(req.MarketName, "-")
+	currency := currencies[0]
+
+	balRequest := bp.GetUserBalanceRequest{
+		UserId:   req.UserId,
+		ApiKeyId: req.ApiKeyId,
+		Currency: currency,
+	}
+
+	balResponse, err := service.Client.GetUserBalance(ctx, &balRequest)
+	if err != nil {
+		response.Status = "error"
+		response.Message = "ecountered error from GetUserBalance: " + err.Error()
+		// we need to return nil here in order to pass the appropriate status
+		// and message to the client. If we return the err the response in
+		// the client will be nil.
+		return nil
+	}
+
+	if balResponse.Data.Balance.Available < req.CurrencyQuantity {
+		response.Status = "fail"
+		response.Message = "sell quantity is greater than available balance for " + currency
+		return nil
+	}
+
+	order, error := orderRepo.InsertOrder(service.DB, req)
+
+	switch {
+	case error == nil:
+		// process order here
+		orderEvent := evt.OrderEvent{
+			Exchange:     "Binance",
+			OrderId:      order.OrderId,
+			UserId:       order.UserId,
+			ApiKeyId:     order.ApiKeyId,
+			MarketName:   order.MarketName,
+			BaseQuantity: order.BaseQuantity,
+			Side:         order.Side,
+			Conditions:   order.Conditions,
+			Status:       "pending",
+		}
+
+		if err := service.NewOrderPub.Publish(context.Background(), &orderEvent); err != nil {
+			log.Println("publish warning: ", err, orderEvent)
+		}
+
+		response.Status = "success"
+		response.Data = &pb.UserOrderData{
+			Order: order,
+		}
+		return nil
+
+	default:
+		response.Status = "error"
+		response.Message = "ecountered error on Insert: " + error.Error()
+		return nil
+	}
+}
+
+func (service *OrderService) AddOrder(ctx context.Context, req *pb.OrderRequest, response *pb.OrderResponse) error {
+	side := enums.NewSideFromString(req.Side)
+
+	switch side {
+	case enums.Buy:
+		return service.addBuyOrder(ctx, req, response)
+	case enums.Sell:
+		return service.addSellOrder(ctx, req, response)
+	default:
+		response.Status = "fail"
+		response.Message = "side unknown: " + req.Side
 		return nil
 	}
 }
