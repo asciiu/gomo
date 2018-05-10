15b340a710e88f6de0aa3bd64778a1d6e9cd0778
Flowmaster <ellyssin.gimhae@gmail.com>
2018-05-10 15:54:23 -0600

    Gracefully handle when the client closes the websocket connection

--- api/controllers/websocketController.go
+++ api/controllers/websocketController.go
@@ -32,28 +32,38 @@ func (controller *WebsocketController) Connect(c echo.Context) error {
 	if err != nil {
 		return err
 	}
-	//defer ws.Close()
+	defer ws.Close()
 
-	controller.connections = append(controller.connections, ws)
-	if err := ws.WriteMessage(websocket.TextMessage, []byte("what's the frequency!")); err != nil {
-		c.Logger().Error(err)
+	if err := ws.WriteMessage(websocket.TextMessage, []byte("ready!")); err != nil {
+		log.Println("write:", err)
+		return err
 	}
+	i := len(controller.connections)
+	controller.connections = append(controller.connections, ws)
+
+	// block until client closes
+	if _, _, err := ws.ReadMessage(); err != nil {
+		// client closes this will read: websocket: close 1005 (no status)
+		log.Println("read:", err)
+	}
+
+	// remove the connection from the connect pool
+	controller.connections = append(controller.connections[:i], controller.connections[i+1:]...)
 	return nil
 }
 
 // ProcessEvent will process ExchangeEvents. These events are published from the exchange sockets.
 func (controller *WebsocketController) ProcessEvent(ctx context.Context, event *evt.ExchangeEvent) error {
-	//log.Println(event)
-
 	for _, conn := range controller.connections {
 		json, err := json.Marshal(event)
 		if err != nil {
 			log.Println(err)
-			return nil
+			return err
 		}
 
 		if err := conn.WriteMessage(websocket.TextMessage, json); err != nil {
 			log.Println(err)
+			return err
 		}
 	}
 
