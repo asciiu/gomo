8a8f75f1dadeeb8c5ce9e56833dc9841a34267c0
Flowmaster <ellyssin.gimhae@gmail.com>
2018-05-15 00:08:06 -0600

    Lock access around market write in search controller

--- api/controllers/searchController.go
+++ api/controllers/searchController.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"fmt"
+	"net/http"
+	"sync"
 
 	evt "github.com/asciiu/gomo/common/proto/events"
 	"github.com/labstack/echo"
@@ -9,25 +11,30 @@ import (
 )
 
 type SearchController struct {
-	buffer map[string]*evt.TradeEvent
+	markets map[string]*evt.TradeEvent
+	mux     sync.Mutex
 }
 
 func NewSearchController() *SearchController {
 	return &SearchController{
-		buffer: make(map[string]*evt.TradeEvent),
+		markets: make(map[string]*evt.TradeEvent),
 	}
 }
 
 func (controller *SearchController) Search(c echo.Context) error {
-	return nil
+
+	name := c.QueryParam("name")
+	return c.String(http.StatusOK, name)
 }
 
 // ProcessEvent will process ExchangeEvents. These events are published from the exchange sockets.
 func (controller *SearchController) ProcessEvent(ctx context.Context, event *evt.TradeEvent) error {
 
 	key := fmt.Sprintf("%s-%s", event.Exchange, event.MarketName)
-	fmt.Println(key)
-	controller.buffer[key] = event
+	//key = key
+	controller.mux.Lock()
+	controller.markets[key] = event
+	controller.mux.Unlock()
 
 	return nil
 }
