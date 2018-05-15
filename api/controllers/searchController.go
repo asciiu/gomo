e4cabb5e18d3d8f72026e496a1f2462206e1ab7d
Flowmaster <ellyssin.gimhae@gmail.com>
2018-05-15 00:46:01 -0600

    Return list of markets in search response

--- api/controllers/searchController.go
+++ api/controllers/searchController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"sync"
 
 	evt "github.com/asciiu/gomo/common/proto/events"
@@ -15,6 +16,13 @@ type SearchController struct {
 	mux     sync.Mutex
 }
 
+// A ResponseSearchSuccess will always contain a status of "successful".
+// swagger:model responseDeviceSuccess
+type ResponseSearchSuccess struct {
+	Status string            `json:"status"`
+	Data   []*evt.TradeEvent `json:"data"`
+}
+
 func NewSearchController() *SearchController {
 	return &SearchController{
 		markets: make(map[string]*evt.TradeEvent),
@@ -23,17 +31,37 @@ func NewSearchController() *SearchController {
 
 func (controller *SearchController) Search(c echo.Context) error {
 
-	name := c.QueryParam("name")
-	return c.String(http.StatusOK, name)
+	term := c.QueryParam("term")
+	m := make([]*evt.TradeEvent, 0)
+
+	for k, v := range controller.markets {
+		if strings.Contains(strings.ToLower(k), term) {
+			m = append(m, v)
+		}
+	}
+
+	response := &ResponseSearchSuccess{
+		Status: "success",
+		Data:   m,
+	}
+
+	return c.JSON(http.StatusOK, response)
 }
 
 // ProcessEvent will process ExchangeEvents. These events are published from the exchange sockets.
 func (controller *SearchController) ProcessEvent(ctx context.Context, event *evt.TradeEvent) error {
+	// shorten trade event
+	tevent := evt.TradeEvent{
+		Exchange:   event.Exchange,
+		Type:       event.Type,
+		MarketName: event.MarketName,
+		Price:      event.Price,
+	}
 
 	key := fmt.Sprintf("%s-%s", event.Exchange, event.MarketName)
 	//key = key
 	controller.mux.Lock()
-	controller.markets[key] = event
+	controller.markets[key] = &tevent
 	controller.mux.Unlock()
 
 	return nil
