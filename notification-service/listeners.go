8247a54e8432170fc013fe92796ffb5dd4f6f651
Flowmaster <ellyssin.gimhae@gmail.com>
2018-05-16 19:31:46 -0600

    The notification listener will send the push notification upon receiving notification events

--- notification-service/listeners.go
+++ notification-service/listeners.go
@@ -3,18 +3,67 @@ package main
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/appleboy/gorush/rpc/proto"
 	notification "github.com/asciiu/gomo/notification-service/proto"
+	"google.golang.org/grpc"
 )
 
 type NotificationListener struct {
-	DB *sql.DB
+	DB     *sql.DB
+	client proto.GorushClient
+	topic  string
+}
+
+func NewNotificationListener(db *sql.DB) *NotificationListener {
+	address := fmt.Sprintf("%s", os.Getenv("GORUSH_ADDRESS"))
+	topic := fmt.Sprintf("%s", os.Getenv("APNS_TOPIC"))
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	client := proto.NewGorushClient(conn)
+
+	listener := NotificationListener{
+		DB:     db,
+		client: client,
+		topic:  topic,
+	}
+
+	return &listener
 }
 
 func (listener *NotificationListener) Process(ctx context.Context, note *notification.Notification) error {
+	// get device tokens from DB for user ID
 	log.Println("notification ", note.Description)
 
-	//_, error := repo.UpdateKeyStatus(listener.DB, key)
+	// loop over device tokens and send
+	r, err := listener.client.Send(context.Background(), &proto.NotificationRequest{
+		Platform: 1,
+		Tokens:   []string{"d98b367c3cdb9d2c2a52a4fe0cc40f95c693ac0d87e7c4fb41988afc3d46111c"},
+		Message:  "test message",
+		Badge:    1,
+		Category: "test",
+		Sound:    "test",
+		Topic:    listener.topic,
+		Alert: &proto.Alert{
+			Title:    "Test Title",
+			Body:     note.Description,
+			Subtitle: "Test Alert Sub Title",
+			LocKey:   "Test loc key",
+			LocArgs:  []string{"test", "test"},
+		},
+	})
+	if err != nil {
+		log.Printf("could not send: %v\n", err)
+	}
+	log.Printf("Success: %t\n", r.Success)
+	log.Printf("Count: %d\n", r.Counts)
+
 	return nil
 }
--- notification-service/main.go
+++ notification-service/main.go
@@ -1,52 +1,15 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"os"
 
-	"github.com/appleboy/gorush/rpc/proto"
 	"github.com/asciiu/gomo/common/db"
 	msg "github.com/asciiu/gomo/common/messages"
 	notification "github.com/asciiu/gomo/notification-service/proto"
 	micro "github.com/micro/go-micro"
-	"google.golang.org/grpc"
 )
-
-func TestPush() {
-	address := fmt.Sprintf("%s", os.Getenv("GORUSH_ADDRESS"))
-
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-	c := proto.NewGorushClient(conn)
-
-	r, err := c.Send(context.Background(), &proto.NotificationRequest{
-		Platform: 1,
-		Tokens:   []string{"d98b367c3cdb9d2c2a52a4fe0cc40f95c693ac0d87e7c4fb41988afc3d46111c"},
-		Message:  "test message",
-		Badge:    1,
-		Category: "test",
-		Sound:    "test",
-		Topic:    "com.mozzarello.projectfomo",
-		Alert: &proto.Alert{
-			Title:    "Test Title",
-			Body:     "Test Alert Body",
-			Subtitle: "Test Alert Sub Title",
-			LocKey:   "Test loc key",
-			LocArgs:  []string{"test", "test"},
-		},
-	})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-	log.Printf("Success: %t\n", r.Success)
-	log.Printf("Count: %d\n", r.Counts)
-}
 
 func main() {
 	srv := micro.NewService(
@@ -66,11 +29,9 @@ func main() {
 
 	notification.RegisterNotificationServiceHandler(srv.Server(), &notificationService)
 
-	listener1 := NotificationListener{gomoDB}
+	listener1 := NewNotificationListener(gomoDB)
 	// handles key verified events
 	micro.RegisterSubscriber(msg.TopicNotification, srv.Server(), &listener1)
-
-	TestPush()
 
 	if err := srv.Run(); err != nil {
 		log.Fatal(err)
