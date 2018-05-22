de508e665f456e94b8720f9df1abbd76cc24550c
Flowmaster <ellyssin.gimhae@gmail.com>
2018-05-22 00:26:56 -0600

    Terraform stuff for google cloud

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "gcloud-region" {
+  default = "us-west1"
+}
+
+variable "gcloud-zone" {
+  default = "us-west1-a"
+}
+
+variable "gcloud-project" {
+  default = "fomo-204904"
+}
+
+variable "platform-name" {
+  default = "fomo-platform"
+}
