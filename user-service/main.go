748dbd8ca22ceccbe38867be456eee35901bd8e5
Flowmaster <ellyssin.gimhae@gmail.com>
2018-05-27 18:36:38 -0600

    Added kube deploy for user service and renamed the proto package name

--- user-service/main.go
+++ user-service/main.go
@@ -8,11 +8,12 @@ import (
 	"github.com/asciiu/gomo/common/db"
 	pb "github.com/asciiu/gomo/user-service/proto/user"
 	micro "github.com/micro/go-micro"
+	k8s "github.com/micro/kubernetes/go/micro"
 )
 
 func NewUserService(name, dbUrl string) micro.Service {
 	// Create a new service. Include some options here.
-	srv := micro.NewService(
+	srv := k8s.NewService(
 		// This name must match the package name given in your protobuf definition
 		micro.Name(name),
 		micro.Version("latest"),
@@ -39,7 +40,7 @@ func NewUserService(name, dbUrl string) micro.Service {
 
 func main() {
 	dbUrl := fmt.Sprintf("%s", os.Getenv("DB_URL"))
-	srv := NewUserService("go.srv.user-service", dbUrl)
+	srv := NewUserService("fomo.users", dbUrl)
 
 	if err := srv.Run(); err != nil {
 		log.Fatal(err)
