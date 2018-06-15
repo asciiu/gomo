a3a69135a132d610aa56acc2205f9bf4e33137df
Flowmaster <ellyssin.gimhae@gmail.com>
2018-06-14 23:57:19 -0600

    Execution engine processor shall receive a payload of trade events.

--- execution-engine/processor.go
+++ execution-engine/processor.go
@@ -22,49 +22,50 @@ type Processor struct {
 }
 
 // ProcessEvent will process ExchangeEvents. These events are published from the exchange sockets.
-func (processor *Processor) ProcessEvent(ctx context.Context, event *evt.TradeEvent) error {
+func (processor *Processor) ProcessEvent(ctx context.Context, payload *evt.TradeEvents) error {
 	//log.Println("buy recv: ", event)
 	orders := processor.Receiver.Orders
 
 	for i, order := range orders {
+		for _, event := range payload.Events {
+			marketName := order.EventOrigin.MarketName
+			//marketName := strings.Replace(order.EventOrigin.MarketName, "-", "", 1)
+			// market name and exchange must match
+			if marketName != event.MarketName || order.EventOrigin.Exchange != event.Exchange {
+				continue
+			}
 
-		marketName := order.EventOrigin.MarketName
-		//marketName := strings.Replace(order.EventOrigin.MarketName, "-", "", 1)
-		// market name and exchange must match
-		if marketName != event.MarketName || order.EventOrigin.Exchange != event.Exchange {
-			continue
-		}
+			conditions := order.Conditions
+			// eval all conditions for this order
+			for _, evaluateFunc := range conditions {
 
-		conditions := order.Conditions
-		// eval all conditions for this order
-		for _, evaluateFunc := range conditions {
+				// does condition of order eval to true?
+				if isValid, desc := evaluateFunc(event.Price); isValid {
+					// remove this order from the processor
+					processor.Receiver.Orders = append(orders[:i], orders[i+1:]...)
 
-			// does condition of order eval to true?
-			if isValid, desc := evaluateFunc(event.Price); isValid {
-				// remove this order from the processor
-				processor.Receiver.Orders = append(orders[:i], orders[i+1:]...)
+					event := order.EventOrigin
+					event.Condition = desc
 
-				event := order.EventOrigin
-				event.Condition = desc
+					switch {
+					case order.EventOrigin.OrderType == types.VirtualOrder:
+						event.ExchangeOrderID = types.VirtualOrder
+						event.ExchangeMarketName = types.VirtualOrder
+						event.Status = status.Filled
 
-				switch {
-				case order.EventOrigin.OrderType == types.VirtualOrder:
-					event.ExchangeOrderID = types.VirtualOrder
-					event.ExchangeMarketName = types.VirtualOrder
-					event.Status = status.Filled
+						// Never log the secrets contained in the event
+						log.Printf("virtual order processed -- orderID: %s, market: %s\n", event.OrderID, event.MarketName)
 
-					// Never log the secrets contained in the event
-					log.Printf("virtual order processed -- orderID: %s, market: %s\n", event.OrderID, event.MarketName)
-
-					if err := processor.Filled.Publish(ctx, event); err != nil {
-						log.Println("publish warning: ", err, event)
-					}
-				default:
-					// Never log the secrets contained in the event
-					log.Printf("order processed -- orderID: %s, market: %s\n", event.OrderID, event.MarketName)
-					// if non simulated trigger buy event - exchange service subscribes to these events
-					if err := processor.Filler.Publish(ctx, event); err != nil {
-						log.Println("publish warning: ", err)
+						if err := processor.Filled.Publish(ctx, event); err != nil {
+							log.Println("publish warning: ", err, event)
+						}
+					default:
+						// Never log the secrets contained in the event
+						log.Printf("order processed -- orderID: %s, market: %s\n", event.OrderID, event.MarketName)
+						// if non simulated trigger buy event - exchange service subscribes to these events
+						if err := processor.Filler.Publish(ctx, event); err != nil {
+							log.Println("publish warning: ", err)
+						}
 					}
 				}
 			}
