974d34f55edadb94c87d7466fc7d8e5db8472cc5
Flowmaster <ellyssin.gimhae@gmail.com>
2018-06-16 18:02:49 -0600

    Added nonentity to response list

--- common/constants/response/response.go
+++ common/constants/response/response.go
@@ -2,7 +2,8 @@ package response
 
 const (
 	// order sides
-	Error   string = "error"
-	Fail    string = "fail"
-	Success string = "success"
+	Error     string = "error"
+	Fail      string = "fail"
+	Success   string = "success"
+	Nonentity string = "nonentity"
 )
