4c6594ccc29880e3f169fdb5d86d3a7e7af35423
Flowmaster <ellyssin.gimhae@gmail.com>
2018-06-17 23:53:11 -0600

    Added failed chain status.

--- common/constants/chain/status.go
+++ common/constants/chain/status.go
@@ -6,6 +6,8 @@ const (
 	Active string = "active"
 	// the chain of orders is not running
 	Inactive string = "inactive"
+	// the chain failed
+	Failed string = "failed"
 	// wtf is this, a learning center for ants!
 	UnknownChainStatus string = "???"
 )
