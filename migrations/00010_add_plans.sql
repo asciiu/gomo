8c5d36cdcaccbfd6b1aa620071de3147437f9807
Flowmaster <ellyssin.gimhae@gmail.com>
2018-06-23 21:43:53 -0600

    plans shall have plan_orders

--- migrations/00010_add_plans.sql
+++ migrations/00010_add_plans.sql
@@ -6,14 +6,28 @@ CREATE TABLE plans (
  user_key_id UUID NOT NULL REFERENCES user_keys (id) ON DELETE CASCADE,
  exchange_name text NOT NULL,
  market_name text NOT NULL,
- order_ids uuid[] NOT NULL,
+ plan_order_ids uuid[] NOT NULL,
  base_balance decimal NOT NULL,
  currency_balance decimal NOT NULL,
- status text NOT NULL,               -- strategy status is active, inactive, or failed
+ status text NOT NULL,               -- plan status is active, inactive, or failed
+ created_on TIMESTAMP DEFAULT now(),
+ updated_on TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE plan_orders (
+ id UUID PRIMARY KEY NOT NULL,
+ plan_id UUID NOT NULL REFERENCES plans (id) ON DELETE CASCADE,
+ base_percent decimal DEFAULT 0,
+ currency_percent decimal DEFAULT 0,
+ side text NOT NULL,
+ conditions jsonb NOT NULL,
+ price decimal DEFAULT 0,
+ status text NOT NULL,               -- pending, active, failed, etc
  created_on TIMESTAMP DEFAULT now(),
  updated_on TIMESTAMP DEFAULT current_timestamp
 );
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
+DROP TABLE plan_orders;
 DROP TABLE plans;
