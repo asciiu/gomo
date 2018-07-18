e272fddeb6eee3ecc413c695fb785431b32904e6
Flowmaster <ellyssin.gimhae@gmail.com>
2018-07-18 17:43:31 -0600

    Added intervals to common

---
+++ common/constants/interval/interval.go
@@ -0,0 +1,12 @@
+package interval
+
+// key status
+const (
+	OneMinute    string = "1m"
+	FiveMinute   string = "5m"
+	TenMinute    string = "10m"
+	ThrityMinute string = "30m"
+	OneHour      string = "1h"
+	TwoHour      string = "2h"
+	OneDay       string = "1d"
+)
