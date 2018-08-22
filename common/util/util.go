e20457555890884936672a753b3bcba7882866c1
Flowmaster <ellyssin.gimhae@gmail.com>
2018-08-21 19:31:24 -0600

    ToFixed should not round up

--- common/util/util.go
+++ common/util/util.go
@@ -4,11 +4,7 @@ import (
 	"math"
 )
 
-func round(num float64) int {
-	return int(num + math.Copysign(0.5, num))
-}
-
 func ToFixed(num float64, precision int) float64 {
 	output := math.Pow(10, float64(precision))
-	return float64(round(num*output)) / output
+	return float64(math.Floor(num*output)) / output
 }
