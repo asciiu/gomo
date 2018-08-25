1319b11e43a10ec954385842d5328ada3a9854f4
Flowmaster <ellyssin.gimhae@gmail.com>
2018-08-25 15:01:00 -0600

    Added constants for paper exchanges.

--- common/constants/exchange/exhange.go
+++ common/constants/exchange/exhange.go
@@ -1,10 +1,20 @@
 package exchange
 
+// all exchange names will be defined here
+// expect the values of these to appear as is to the user
 const (
 	// exchange names
-	Binance         string = "binance"
-	Bittrex         string = "bittrex"
-	Poloniex        string = "poloniex"
-	Kucoin          string = "kucoin"
+	Binance      string = "binance"
+	BinancePaper string = "binance paper"
+
+	Bittrex      string = "bittrex"
+	BittrexPaper string = "bittrex paper"
+
+	Poloniex      string = "poloniex"
+	PoloniexPaper string = "poloniex paper"
+
+	Kucoin      string = "kucoin"
+	KucoinPaper string = "kucoin paper"
+
 	UnknownExchange string = "???"
 )
