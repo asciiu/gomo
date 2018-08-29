112a64dce544d47d455a8545d7c7563afffec14c
Flowmaster <ellyssin.gimhae@gmail.com>
2018-08-28 22:57:11 -0600

    Correct type for initial timestamp.

--- migrations/00016_add_plan_init_timestamp.sql
+++ migrations/00016_add_plan_init_timestamp.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- this shall be the currency used to measure initial and final currency valuations 
-ALTER TABLE plans ADD COLUMN initial_timestamp text; 
+ALTER TABLE plans ADD COLUMN initial_timestamp TIMESTAMP; 
 
 -- +goose Down
 ALTER TABLE plans DROP COLUMN initial_timestamp;
