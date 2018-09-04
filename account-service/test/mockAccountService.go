8a9a8c29f44c310109fd6838ab51ad3b4b059141
Flowmaster <ellyssin.gimhae@gmail.com>
2018-09-03 19:35:39 -0600

    Added the mock account service for test.

---
+++ account-service/test/mockAccountService.go
@@ -0,0 +1,97 @@
+package test
+
+import (
+	"context"
+	"database/sql"
+
+	protoAccount "github.com/asciiu/gomo/account-service/proto/account"
+	protoBalance "github.com/asciiu/gomo/account-service/proto/balance"
+	"github.com/micro/go-micro/client"
+)
+
+// Test clients of the Key service should use this client interface.
+type mockAccountService struct {
+	db *sql.DB
+}
+
+func (m *mockAccountService) AddAccount(ctx context.Context, req *protoAccount.NewAccountRequest, opts ...client.CallOption) (*protoAccount.AccountResponse, error) {
+	return &protoAccount.AccountResponse{
+		Status: "success",
+	}, nil
+}
+
+func (m *mockAccountService) AddBalance(ctx context.Context, req *protoBalance.NewBalanceRequest, opts ...client.CallOption) (*protoBalance.BalanceResponse, error) {
+	return &protoBalance.BalanceResponse{
+		Status: "success",
+	}, nil
+}
+
+func (m *mockAccountService) AdjustAvailableBalance(ctx context.Context, req *protoBalance.AdjustBalanceRequest, opts ...client.CallOption) (*protoBalance.BalanceResponse, error) {
+	return &protoBalance.BalanceResponse{
+		Status: "success",
+	}, nil
+}
+
+func (m *mockAccountService) AdjustLockedBalance(ctx context.Context, req *protoBalance.AdjustBalanceRequest, opts ...client.CallOption) (*protoBalance.BalanceResponse, error) {
+	return &protoBalance.BalanceResponse{
+		Status: "success",
+	}, nil
+}
+
+func (m *mockAccountService) AdjustBalance(ctx context.Context, req *protoBalance.AdjustBalanceRequest, opts ...client.CallOption) (*protoBalance.BalanceResponse, error) {
+	return &protoBalance.BalanceResponse{
+		Status: "success",
+	}, nil
+}
+
+func (m *mockAccountService) DeleteAccount(ctx context.Context, req *protoAccount.AccountRequest, opts ...client.CallOption) (*protoAccount.AccountResponse, error) {
+	return &protoAccount.AccountResponse{
+		Status: "success",
+	}, nil
+}
+
+func (m *mockAccountService) GetAccounts(ctx context.Context, req *protoAccount.AccountsRequest, opts ...client.CallOption) (*protoAccount.AccountsResponse, error) {
+	return &protoAccount.AccountsResponse{
+		Status: "success",
+	}, nil
+}
+
+func (m *mockAccountService) GetAccount(ctx context.Context, req *protoAccount.AccountRequest, opts ...client.CallOption) (*protoAccount.AccountResponse, error) {
+	return &protoAccount.AccountResponse{
+		Status: "success",
+	}, nil
+}
+
+func (m *mockAccountService) GetAccountKeys(ctx context.Context, req *protoAccount.GetAccountKeysRequest, opts ...client.CallOption) (*protoAccount.AccountKeysResponse, error) {
+	return &protoAccount.AccountKeysResponse{
+		Status: "success",
+	}, nil
+}
+
+func (m *mockAccountService) GetAccountBalance(ctx context.Context, req *protoBalance.BalanceRequest, opts ...client.CallOption) (*protoBalance.BalanceResponse, error) {
+	return &protoBalance.BalanceResponse{
+		Status: "success",
+	}, nil
+}
+
+func (m *mockAccountService) ResyncAccounts(ctx context.Context, req *protoAccount.AccountsRequest, opts ...client.CallOption) (*protoAccount.AccountsResponse, error) {
+	return &protoAccount.AccountsResponse{
+		Status: "success",
+	}, nil
+}
+
+func (m *mockAccountService) UpdateAccount(ctx context.Context, req *protoAccount.UpdateAccountRequest, opts ...client.CallOption) (*protoAccount.AccountResponse, error) {
+	return &protoAccount.AccountResponse{
+		Status: "success",
+	}, nil
+}
+
+func (m *mockAccountService) ValidateAccountBalance(ctx context.Context, req *protoBalance.ValidateBalanceRequest, opts ...client.CallOption) (*protoBalance.ValidateBalanceResponse, error) {
+	return &protoBalance.ValidateBalanceResponse{
+		Status: "success",
+	}, nil
+}
+
+func MockAccountServiceClient(db *sql.DB) protoAccount.AccountServiceClient {
+	return &mockAccountService{db}
+}
