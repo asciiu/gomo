82cba0bd2193282e24109b61ed684a2c63cad084
Flowmaster <ellyssin.gimhae@gmail.com>
2018-10-02 00:08:58 -0600

    Added migration for new order result columns - fee_currency_symbol, fee_currency_amount, average_price, exchange_timestamp, and errors.

---
+++ migrations/00024_add_order_results.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- this is needed because some currency symbols are different accross exchanges
+-- example: BCH is BCC on binance
+ALTER TABLE orders ADD COLUMN fee_currency_symbol text;
+ALTER TABLE orders ADD COLUMN fee_currency_amount decimal DEFAULT 0;
+ALTER TABLE orders ADD COLUMN average_price decimal DEFAULT 0;
+ALTER TABLE orders ADD COLUMN exchange_timestamp TIMESTAMP;
+ALTER TABLE orders ADD COLUMN errors text;
+
+-- +goose Down
+ALTER TABLE orders DROP COLUMN fee_currency_symbol; 
+ALTER TABLE orders DROP COLUMN fee_currency_amount; 
+ALTER TABLE orders DROP COLUMN average_price; 
+ALTER TABLE orders DROP COLUMN exchange_timestamp; 
+ALTER TABLE orders DROP COLUMN errors; 
