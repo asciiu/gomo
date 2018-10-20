2e4d022761c83dd525977fb18aed2897558f53d3
Flowmaster <ellyssin.gimhae@gmail.com>
2018-10-19 20:58:39 -0600

    There shall only be one Null object.

--- analytics-service/fomolang/evaluator/evaluator.go
+++ analytics-service/fomolang/evaluator/evaluator.go
@@ -6,6 +6,7 @@ import (
 )
 
 var (
+	NULL  = &object.Null{}
 	TRUE  = &object.Boolean{Value: true}
 	FALSE = &object.Boolean{Value: false}
 )
@@ -22,6 +23,7 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+
 	}
 	return nil
 }
