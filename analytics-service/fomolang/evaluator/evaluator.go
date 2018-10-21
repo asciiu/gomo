492fb57288384159da918fdc7bb2a393dd8ca9c2
Flowmaster <ellyssin.gimhae@gmail.com>
2018-10-20 21:33:30 -0600

    Eval should be able to handle prefix expressions.

--- analytics-service/fomolang/evaluator/evaluator.go
+++ analytics-service/fomolang/evaluator/evaluator.go
@@ -23,9 +23,33 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
-
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 	return nil
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
